1709e6e01d3dc3f1953b76e99bcafd9caaf7ac2b
thalles <thalles63@gmail.com>
2021-04-25 22:50:31 -0300

    Ajustes mobile layout

--- js/main.js
+++ js/main.js
@@ -3,24 +3,24 @@
 })();
 const year = document.querySelector("#year");
 year.innerHTML = new Date().getFullYear();
-function toggleMenu() {
-   const menu = document.querySelector('.hamburger');
-   const navigation = document.querySelector('.navigation');
-   const header = document.querySelector('header');
+// function toggleMenu() {
+//    const menu = document.querySelector('.hamburger');
+//    const navigation = document.querySelector('.navigation');
+//    const header = document.querySelector('header');
 
-   menu.classList.toggle('is-active');
-   navigation.classList.toggle('active');
-   header.classList.toggle('active');
-}
-function closeMenu() {
-   const menu = document.querySelector('.hamburger');
-   const navigation = document.querySelector('.navigation');
-   const header = document.querySelector('header');
+//    menu.classList.toggle('is-active');
+//    navigation.classList.toggle('active');
+//    header.classList.toggle('active');
+// }
+// function closeMenu() {
+//    const menu = document.querySelector('.hamburger');
+//    const navigation = document.querySelector('.navigation');
+//    const header = document.querySelector('header');
 
-   menu.classList.remove('is-active');
-   navigation.classList.remove('active');
-   header.classList.remove('active');
-}
+//    menu.classList.remove('is-active');
+//    navigation.classList.remove('active');
+//    header.classList.remove('active');
+// }
 window.addEventListener('scroll', function () {
    const header = document.querySelector('header');
    header.classList.toggle("sticky", window.scrollY > 0)
@@ -30,7 +30,6 @@ function goTo(section) {
       behavior: 'smooth'
    });
 }
-
 window.onload = function () {
    document.getElementById('contact-form').addEventListener('submit', function (event) {
       event.preventDefault();
